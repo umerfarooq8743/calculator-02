55f83365041f02a25faeb022138f91c1f6129472
Umer Farooq <64475488+umerfarooq8743@users.noreply.github.com>
2022-12-22 13:14:49 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+
+function getNumber(num){
+    var result = document.getElementById("result")
+    result.value += num
+    }
+    
+function clearResult(){
+        var result = document.getElementById("result")
+        result.value = ""
+    }
+
+function getResult(){
+     var result = document.getElementById("result")
+     result.value = eval(result.value)
+}
+
+function editBtn(){
+    var result = document.getElementById("result")
+    result.value = result.value.slice(0,-1)
+}
